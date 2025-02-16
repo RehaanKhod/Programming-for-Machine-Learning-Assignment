d6748f38672e0d8b70ce5b736babca7b32c1f4d5
Rehaan Khodabocus <rehaan.khodabocus@umail.utm.ac.mu>
2025-02-16 18:37:05 +0400

    Update MaxTemp_AcrossWeatherTypes.py

--- MaxTempAcrossWeatherTypes/MaxTemp_AcrossWeatherTypes.py
+++ MaxTempAcrossWeatherTypes/MaxTemp_AcrossWeatherTypes.py
@@ -1,1 +1,8 @@
-
+# Max Temperature Across Weather Types - Shows the temperature range for different weather conditions & detect outliers(ex:  unusually high/low temps)
+plt.figure(figsize=(10, 6))
+sns.boxplot(data=df, x="weather", y="temp_max")
+plt.xticks(rotation=45)  # Rotate labels for better readability
+plt.xlabel("Weather Type")
+plt.ylabel("Max Temperature (°C)")
+plt.title("Max Temperature Distribution Across Weather Types")
+plt.show()

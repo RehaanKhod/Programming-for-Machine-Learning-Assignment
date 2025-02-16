c20056823be0ce37b561fec319cdf63ab870262c
Rehaan Khodabocus <rehaan.khodabocus@umail.utm.ac.mu>
2025-02-16 18:38:17 +0400

    Moved Correletion Heatmap/PairPlot_WeatherFeatures.py to folder

--- Correletion Heatmap/PairPlot_WeatherFeatures.py
+++
@@ -1,7 +0,0 @@
-#Pair Plot: Relationships Between Weather Variables
-# Selecting relevant numerical columns
-numeric_features = ["temp_max", "temp_min", "precipitation", "wind"]
-# Create pair plot
-sns.pairplot(df[numeric_features], diag_kind="kde", markers="o", plot_kws={'alpha':0.6})
-plt.suptitle("Pair Plot of Weather Features", y=1.02)
-plt.show()

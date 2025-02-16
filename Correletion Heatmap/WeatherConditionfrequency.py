0d68773e6d4180a1332cef5b13b2cbe6e080adf5
Rehaan Khodabocus <rehaan.khodabocus@umail.utm.ac.mu>
2025-02-16 18:42:19 +0400

    Moved Correletion Heatmap/WeatherConditionfrequency.py to folder

--- Correletion Heatmap/WeatherConditionfrequency.py
+++
@@ -1,6 +0,0 @@
-# Weather condition frequency -  A bar chart depicting the count of occurrences for each weather condition.
-plt.figure(figsize=(10, 6))
-sns.countplot(x=df['weather'], order=df['weather'].value_counts().index, palette='coolwarm')
-plt.xticks(rotation=45)
-plt.title("Weather Condition Frequency")
-plt.show()

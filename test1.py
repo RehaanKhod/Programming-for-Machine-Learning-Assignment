153b1be6fb27fbd324e3cb919ac15371ea4fe9bb
Rehaan Khodabocus <rehaan.khodabocus@umail.utm.ac.mu>
2025-02-18 21:28:16 +0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -71,5 +71,11 @@ ax.set_title("Max and Min Temperatures Over Time")
 ax.legend()
 st.pyplot(fig)
 
-
+#Pair Plot: Relationships Between Weather Variables
+# Selecting relevant numerical columns
+numeric_features = ["temp_max", "temp_min", "precipitation", "wind"]
+# Create pair plot
+sns.pairplot(df[numeric_features], diag_kind="kde", markers="o", plot_kws={'alpha':0.6})
+plt.suptitle("Pair Plot of Weather Features", y=1.02)
+st.show()
 
